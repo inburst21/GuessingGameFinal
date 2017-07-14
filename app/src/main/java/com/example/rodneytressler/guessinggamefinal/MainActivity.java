5fc4a96190def6e304bead5635f4c98542ddce31
Travis40508 <Rodney.Tressler@Interapthq.com>
2017-07-13 21:52:27 -0400

    [LOCATINGFILES] shows location of res and Java files.

--- app/src/main/java/com/example/rodneytressler/guessinggamefinal/MainActivity.java
+++ app/src/main/java/com/example/rodneytressler/guessinggamefinal/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rodneytressler.guessinggamefinal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/** Java class for writing logic for app. */
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
