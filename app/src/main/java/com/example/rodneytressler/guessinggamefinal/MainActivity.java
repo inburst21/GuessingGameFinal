5e8a24f3ab67c7c5671dbfca2e78a044a78c9397
Travis40508 <Rodney.Tressler@Interapthq.com>
2017-07-14 00:00:15 -0400

    [BINDINGVIEWS] binds our submit button in code.

--- app/src/main/java/com/example/rodneytressler/guessinggamefinal/MainActivity.java
+++ app/src/main/java/com/example/rodneytressler/guessinggamefinal/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.rodneytressler.guessinggamefinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 /** Java class for writing logic for app. */
 public class MainActivity extends AppCompatActivity {
 
+  private Button submitButton;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    /** This takes our submit button member variable above, and binds it to our
+     * button UI element in our xml file. This works the same for each element.*/
+
+    submitButton = (Button) findViewById(R.id.button_submit);
   }
 }
