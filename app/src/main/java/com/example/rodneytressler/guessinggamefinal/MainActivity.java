c3d68bd7bc311063db8069614ecdebbc8a094f1e
Travis40508 <Rodney.Tressler@Interapthq.com>
2017-07-14 00:04:00 -0400

    [BINDINGEDITTEXT] this binds our edit text UI element to our Java file.

--- app/src/main/java/com/example/rodneytressler/guessinggamefinal/MainActivity.java
+++ app/src/main/java/com/example/rodneytressler/guessinggamefinal/MainActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /** Java class for writing logic for app. */
 public class MainActivity extends AppCompatActivity {
 
   private Button submitButton;
+  private EditText inputField;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -18,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
      * button UI element in our xml file. This works the same for each element.*/
 
     submitButton = (Button) findViewById(R.id.button_submit);
+
+    /** This binds our edit text member variable to our edit text UI element from our XML file*/
+    inputField = (EditText) findViewById(R.id.input_field);
 
     /** This method will listen for a button press on our submitButton*/
     submitButtonClicked();
