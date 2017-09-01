4be561dd4bc2a1c7b5c61a678c0130576b24ac0d
Travis Tressler <travistressler@Traviss-MacBook-Pro.local>
2017-09-01 10:15:05 -0400

    [TURNS] adds a turn-limit, causing the user to lose if they don't get the answer right after 5 turns.

--- app/src/main/java/com/example/rodneytressler/guessinggamefinal/MainActivity.java
+++ app/src/main/java/com/example/rodneytressler/guessinggamefinal/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
   private Button submitButton;
   private EditText inputField;
   private TextView instructions;
+  private int turnsLeft;
 
 
   /** This is a member variable for our winning number*/
@@ -52,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
     /** Here we generate a random winning number between the range of 1 and 100. We add 1 to the winning
      * value to make sure it can reach 100. */
 
+    turnsLeft = 5;
     winningNumber = 1 + (int)(Math.random() * ((100 - 1) + 1));
 
     /** Here we're using our previously extracted String resource for instructions to set the text when starting
@@ -67,48 +69,61 @@ public class MainActivity extends AppCompatActivity {
   private void submitButtonClicked() {
 
     /** Here we're setting an onClickListener on our button. afterwards, we can specify
-     * what we wish to do when the button is clicked.*/
+     * what we wish to do when the button is clicked. We make our functionality based on the button's text, as we want it to do something
+     * completely differently if the text says 'restart' after running out of turns and losing. */
 
     submitButton.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View view) {
+        if (!submitButton.getText().equals("Restart")) {
 
-        /** Here we're declaring a variable equal to the value inside of our edittext*/
-        String inputText = inputField.getText().toString();
+          /** Here we're declaring a variable equal to the value inside of our edittext*/
+          String inputText = inputField.getText().toString();
 
-        /** Now we're toasting that value onto the screen!*/
-        Toast.makeText(MainActivity.this, "Checking your guess of " + inputText, Toast.LENGTH_SHORT).show();
 
-        /** Now we'll pass our guess into our checkGuess method, to see if we won. We first have to convert our
-         * string guess into an int so its value can be checked. We do this with Integer.parseInt()*/
-        checkGuess(Integer.parseInt(inputText));
+          /** Now we'll pass our guess into our checkGuess method, to see if we won. We first have to convert our
+           * string guess into an int so its value can be checked. We do this with Integer.parseInt()*/
+          checkGuess(Integer.parseInt(inputText));
 
-        /** This will erase all of the text in our edittext box*/
-        inputField.setText("");
+          /** This will erase all of the text in our edittext box*/
+          inputField.setText("");
+        } else {
+          submitButton.setText("Submit");
+          startGame();
+        }
       }
     });
   }
 
   public void checkGuess(int guess) {
 
-    /** This checks our guess and sets the instructional text view accordingly*/
+    /** This checks our guess and sets the instructional text view accordingly. It decrements the amount of turns the user has left and
+     * changes the text and button text when they run out of turns, which, as we know, changes the functionality of the button.*/
 
-    if(guess > winningNumber) {
-      instructions.setText("Lower");
-    } else if(guess < winningNumber) {
-      instructions.setText("Higher");
-    } else if (guess == winningNumber) {
-      /** Here we're creating our intent. this forms a relationship, a connection between these
-       * two activities. We're calling startGame() to reset all values, in case we want
-       * to play again, as the activity is still running when it's not visible*/
-      Intent intent = new Intent(this, WinnerActivity.class);
+    if (turnsLeft != 1) {
+      turnsLeft--;
+      if (guess > winningNumber) {
+        instructions.setText("Lower");
+        Toast.makeText(this, turnsLeft + " turn(s) left!", Toast.LENGTH_SHORT).show();
+      } else if (guess < winningNumber) {
+        instructions.setText("Higher");
+        Toast.makeText(this, turnsLeft + " turn(s) left!", Toast.LENGTH_SHORT).show();
+      } else if (guess == winningNumber) {
+        /** Here we're creating our intent. this forms a relationship, a connection between these
+         * two activities. We're calling startGame() to reset all values, in case we want
+         * to play again, as the activity is still running when it's not visible*/
+        Intent intent = new Intent(this, WinnerActivity.class);
 
-      /** This is where we'll pass the winning number into the winning activity, so it can set the textview equal to
-       * the winning value*/
-      intent.putExtra(WINNING_NUMBER, winningNumber);
-      startActivity(intent);
-      startGame();
+        /** This is where we'll pass the winning number into the winning activity, so it can set the textview equal to
+         * the winning value*/
+        intent.putExtra(WINNING_NUMBER, winningNumber);
+        startActivity(intent);
+        startGame();
+      } else {
+        Toast.makeText(this, "Invalid Entry, please try again!", Toast.LENGTH_SHORT).show();
+      }
     } else {
-      Toast.makeText(this, "Invalid Entry, please try again!", Toast.LENGTH_SHORT).show();
+      instructions.setText("Game Over! The winning number was " + winningNumber + ". Please try again.");
+      submitButton.setText("Restart");
     }
   }
 
